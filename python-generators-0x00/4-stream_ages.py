3f614c4e189e32a5f51322fb8fc13a6eeb38d6fd
PantheraNestah <pantheranestah@gmail.com>
2025-07-06 22:25:29 +0300

    added script to do average age calculation by leveraging generator and using yield for a memory efficient operation

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+seed = __import__('seed')
+
+def stream_users_ages():
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+    cursor.execute("SELECT age FROM user_data;")
+
+    while True:
+        age = cursor.fetchone()
+        if age is None:
+            break
+        yield age
+
+    cursor.close()
+    connection.close()
+
+def calculate_average_age():
+    total_age = 0
+    count = 0
+
+    for age in stream_users_ages():
+        total_age += age[0]
+        count += 1
+
+    if count == 0:
+        return 0
+
+    return float(total_age / count)
+
+if __name__ == "__main__":
+    average_age = calculate_average_age()
+    print(f"Average age of users: {average_age:.2f}")
