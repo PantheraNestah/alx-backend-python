b22045d0537abdeaab47a5d664e0e3525647dd08
PantheraNestah <pantheranestah@gmail.com>
2025-07-06 18:46:27 +0300

    Created enerator that streams rows from an SQL database one by one

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,13 @@
+seed = __import__('seed')
+
+def stream_users():
+    connection = seed.connect_to_prodev()
+    if connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM user_data;")
+        for row in cursor:
+            yield row
+        cursor.close()
+        connection.close()
+    else:
+        yield None
