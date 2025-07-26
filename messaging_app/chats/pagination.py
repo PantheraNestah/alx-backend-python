9949625f02f8e22f3b256b21c2e9ca54ea8fb554
PantheraNestah <pantheranestah@gmail.com>
2025-07-26 09:11:01 +0300

    feat: enhance pagination response with detailed metadata

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,8 +1,17 @@
 # chats/pagination.py
 
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     page_size = 20 # Number of messages per page
     page_size_query_param = 'page_size' # Allows clients to override page size with ?page_size=X
     max_page_size = 100 # Maximum page size a client can request
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
